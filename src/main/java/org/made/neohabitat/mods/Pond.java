5c4609ea78d706ad9961e8c76dc1015e126f30ab
NeoEdo1819 <matt_post@me.com>
2017-01-29 05:26:32 +0000

    Implemented Pond and Sky classes

---
+++ src/main/java/org/made/neohabitat/mods/Pond.java
@@ -0,0 +1,61 @@
+package org.made.neohabitat.mods;
+
+import org.elkoserver.foundation.json.JSONMethod;
+import org.elkoserver.foundation.json.OptInteger;
+import org.elkoserver.json.EncodeControl;
+import org.elkoserver.json.JSONLiteral;
+import org.made.neohabitat.HabitatMod;
+
+/**
+ * Habitat Sky Mod (attached to an Elko Item.)
+ * 
+ * Ponds just hang around being wet - They only respond to HELP messages. [The client is
+ * supposed to be smart and transform interface commands to *other* objects as
+ * needed.]
+ * 
+ * @author matt
+ *
+ */
+public class Pond extends HabitatMod {
+    
+    public int HabitatClass() {
+        return CLASS_POND;
+    }
+    
+    public String HabitatModName() {
+        return "Pond";
+    }
+    
+    public int capacity() {
+        return 0;
+    }
+    
+    public int pc_state_bytes() {
+        return 0;
+    };
+    
+    public boolean known() {
+        return true;
+    }
+    
+    public boolean opaque_container() {
+        return false;
+    }
+    
+    public boolean filler() {
+        return false;
+    }
+    
+    @JSONMethod({ "style", "x", "y", "orientation", "gr_state" })
+    public Pond(OptInteger style, OptInteger x, OptInteger y, OptInteger orientation, OptInteger gr_state) {
+        super(style, x, y, orientation, gr_state);
+    }
+    
+    @Override
+    public JSONLiteral encode(EncodeControl control) {
+        JSONLiteral result = super.encodeCommon(new JSONLiteral(HabitatModName(), control));
+        result.finish();
+        return result;
+    }
+    
+}
