29984c58a1673487741bed03f003c9649e3aafa7
Steve Salevan <steve.salevan@gmail.com>
2017-02-14 18:07:41 -0800

    Fixing orient/orientation

--- bridge/Habitat2ElkoBridge.js
+++ bridge/Habitat2ElkoBridge.js
@@ -742,10 +742,10 @@ var encodeState = {
 		Chest:		function (state, container, buf) { return (this.openable(state, container, buf)); },
 		Plant: 		function (state, container, buf) { return (this.massive (state, container, buf)); },
 		Flag: 		function (state, container, buf) { return (this.massive (state, container, buf)); },
-		Trapezoid: 	function (state, container, buf) { return (this.polygonal(state, container, buf)); },
+		Trapezoid: 	function (state, container, buf) { return (this.polygonal(state,container, buf)); },
 		Hot_tub:    function (state, container, buf) { return (this.common  (state, container, buf)); },
-		Fountain:   function (state, container, buf) { return (this.common  (state, container, buf)); },
-		Compass:  function(state, container, buf) { return (this.common(state, container, buf)); }
+		Fountain:   function (state, container, buf) { return (this.common  (state, container, buf)); }
+
 };
 
 function habitatEncodeElkoModState (state, container, buf) {
