0f01db60b162b316e05c4deaec7e73b8b72668ec
Steve Salevan <steve.salevan@gmail.com>
2017-03-06 03:18:53 -0800

    Adds support to Regionator for translation of nested mods

--- regionator/parser.py
+++ regionator/parser.py
@@ -4,6 +4,7 @@ from pyparsing import (
     LineEnd,
     LineStart,
     nums,
+    Optional,
     Word,
     ZeroOrMore,
 )
@@ -23,18 +24,35 @@ mod_identifier = Word(alphas.upper() + alphas.lower() + '_')
 mod_param_name = Word(' ' + alphas.upper() + alphas.lower() + '_')
 
 mods_start_line = '[' + LineEnd()
+mods_end_line = ']' + LineEnd()
 
 mod_param = mod_param_name + ':' + Word(nums) + ";"
 mod_param_additional_line = Word(nums) + ':' + Word(nums) + ';'
 
-mod_start = '@' + mod_identifier.setResultsName('mod_identifier') + '{' 
-mod_end = '}' + LineEnd()
+# pyparsing is not a recursive descent parser, so this ugly little hack takes advantage
+# of the fact that there are no more than three layers of nesting in any RDL file.
+inner_mod_3 = ('@' + mod_identifier.setResultsName('inner_mod_3_identifier') + '{' +
+  ZeroOrMore(mod_param).setResultsName('inner_mod_3_params') + 
+  ZeroOrMore(mod_param_additional_line).setResultsName('inner_mod_3_params_additional') +
+  '}')
 
-mod = (mod_start + ZeroOrMore(mod_param).setResultsName('mod_params') + 
+inner_mod_2 = ('@' + mod_identifier.setResultsName('inner_mod_2_identifier') + '{' +
+  ZeroOrMore(mod_param).setResultsName('inner_mod_2_params') + 
+  ZeroOrMore(mod_param_additional_line).setResultsName('inner_mod_2_params_additional') +
+  Optional(mods_start_line + ZeroOrMore(inner_mod_3('inner_mod_3')) + mods_end_line) +
+  '}')
+
+inner_mod_1 = ('@' + mod_identifier.setResultsName('inner_mod_1_identifier') + '{' +
+  ZeroOrMore(mod_param).setResultsName('inner_mod_1_params') + 
+  ZeroOrMore(mod_param_additional_line).setResultsName('inner_mod_1_params_additional') +
+  Optional(mods_start_line + ZeroOrMore(inner_mod_2('inner_mod_2')) + mods_end_line) +
+  '}')
+
+mod = ('@' + mod_identifier.setResultsName('mod_identifier') + '{' +
+  ZeroOrMore(mod_param).setResultsName('mod_params') + 
   ZeroOrMore(mod_param_additional_line).setResultsName('mod_params_additional') +
-  mod_end)
-
-mods_end_line = ']' + LineEnd()
+  Optional(mods_start_line + ZeroOrMore(inner_mod_1('inner_mod_1')) + mods_end_line) +
+  '}')
 
 mods = mods_start_line + ZeroOrMore(mod('mod')) + mods_end_line
 
