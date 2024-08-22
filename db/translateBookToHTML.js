d7b4706b3506159ab78ed9f2cc0c324268c56fe4
randy <randy@spritely.institute>
2024-08-22 14:10:24 -0700

    Parses args, takes input file list and allows for title override in output.

--- db/translateBookToHTML.js
+++ db/translateBookToHTML.js
@@ -3,6 +3,18 @@ const File       = require('fs').promises;
 /** Object for trace library - npm install winston */
 const Trace      = require('winston');
 
+const Argv       = require('yargs')
+        .usage('Usage: node $0 --files=json,file,list [options]')
+        .example('node $0 --files=Text/text-popmap.json', "Translate Habitat Text Documents to HTML documents at the same path.\n")                   
+        .help('help')
+        .option('help',      { alias: '?',                   describe: 'Get this usage/help information.'})
+        .option('name',      { alias: 'n',                   describe: 'Set the name (aka title) of the web page, defaults to first line of document.'})
+        .option('trace',     { alias: 't', default: "debug", describe: 'Trace level name. (see: npm winston)'})
+        .option('files',     { alias: 'f',                   describe: 'List of .json-ish Habitat Text files, comma delimited.'})
+        .argv;
+
+Trace.level      = Argv.trace;
+
 var Translate = []
 
 for (let character = 0; character < 128; character++) {
@@ -14,8 +26,6 @@ Translate[9]  = "&#17;";
 Translate[10] = "&#18;";
 Translate[13] = "&#17;";
 
-var outfile="document.html";
-var infile="text-test.json"; 
 const stringJoinRegex = /(("([^"]|\\")*"\s*\+\s*)+"([^"]|\\")*")/g;
 
 function templateStringJoins(data) {
@@ -42,7 +52,7 @@ function writeHtmlBlock(block) {
 
 function htmlHeader(title) {
   writeHtmlBlock('<html>');
-  writeHtmlBlock('<title>' + title + '</title>');
+  writeHtmlBlock('<title>' + (Argv.name || title) + '</title>');
   writeHtmlBlock('<body style="font-family:charset;font-size:24;">');
   writeHtmlBlock('<link rel="stylesheet" href="https://frandallfarmer.github.io/neohabitat-doc/docs/charset/charset.css" type="text/css" charset="utf-8"/>');
 }
@@ -57,7 +67,7 @@ function htmlFooter() {
   writeHtmlBlock('</body></html>');
 }
 
-const Habitat2HTML = async () => {
+const Habitat2HTML = async (infile) => {
   const jsonish = await File.readFile(infile, 'utf8');
   var document = JSON.parse(templateStringJoins(jsonish));
   var html = "";
@@ -67,7 +77,7 @@ const Habitat2HTML = async () => {
       let html = "";
       for (let pos = 0, len = page.length; pos < len ; pos++) {
         html += Translate[page.charCodeAt(pos)];
-        if (pos > 0 && ((pos + 1) % 40) == 0) { html += "\n" }
+        if (((pos + 1) % 40) == 0) { html += "\n" }
       }
       if (writeHeader) { htmlHeader(html.substring(0,40)); writeHeader = false;}
       writeHabidocPage(html);
@@ -77,7 +87,7 @@ const Habitat2HTML = async () => {
     page = document.ascii[0];
     for (let pos = 0, len = page.length; pos < len ; pos++) {
       html += Translate[page[pos]];
-      if (pos > 0 && ((pos + 1) % 40) == 0) { html += "\n" }
+      if (((pos + 1) % 40) == 0) { html += "\n" }
     }
     htmlHeader(html.substring(0,40));
     writeHabidocPage(html);
@@ -86,6 +96,14 @@ const Habitat2HTML = async () => {
 }
 
 (async function main() {
-  await Habitat2HTML();
+
+  if (Argv.files) {
+    var files = Argv.files.split(',');
+    for (var i = 0; i < files.length; i++) {
+        await Habitat2HTML(files[i]);
+    }
+  } else {
+    console.log("No files specified, nothing done. See --help");
+  }
 }());
 
